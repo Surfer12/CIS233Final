c4aadd4a6f52f2dfa130c872dddc52c99716c48e
Ryan Oates <ryanoatsie@outlook.com>
2024-12-12 12:35:51 -0800

    Remove exam prompt file containing instructions and guidelines for the in-person exam.

---
+++ javafinal/src/qone.java
@@ -0,0 +1,20 @@
+/* Given this code snippet, draw the graph representation and upload a picture of your graph.
+NB: It is a weighted graph
+ */
+
+// TODO Draw this image
+
+
+
+Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+
+         // Add nodes and edges with weights
+         graph.put(1, new HashMap<>(Map.of(2, 4, 3, 2)));
+         graph.put(2, new HashMap<>(Map.of(4, 5, 5, 1)));
+         graph.put(3, new HashMap<>(Map.of(7, 3)));
+         graph.put(4, new HashMap<>());
+         graph.put(5, new HashMap<>());
+         graph.put(6, new HashMap<>());
+         graph.put(7, new HashMap<>(Map.of(8, 2, 9, 4)));
+         graph.put(8, new HashMap<>());
+         graph.put(9, new HashMap<>(Map.of(4, 5, 5, 1)));
