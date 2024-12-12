a2f8546d07f90b275f8c1bc0fe52b4b2999c17ec
Ryan Oates <ryanoatsie@outlook.com>
2024-12-12 13:48:51 -0800

    Refactor BookConnectionRecs class: added enum for book titles, corrected graph initialization syntax, and improved method structure. Fixed issues with hashmap entries and ensured proper functionality in the main method.

--- javafinal/src/BookConnectionRecs.java
+++ javafinal/src/BookConnectionRecs.java
@@ -2,31 +2,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BookConnectionRecs {
-    // create our Hashmap that takes a map Map<Integer, Map<Integer, Integer>>
-
-// Create our method that places the source and destination
-
-// Create our method that displays the results as a java adj list
-
-@Override
-
-    public void put(source int, destination int){
-//Todo override method to place
+    // We utilize an enum to assign our book titles
+    public enum BookConnectionRecsTitles {
+        enum
+{
+'a' = "The Clockwork Dragon"
+'b' = "Whispers of the Ancients"
+'c' = "The Sunken City"
+'d'  = "The Starfire Codex"
+'e' = "The Forgotten King"
+'f' = "Echoes of the Past"
+'h' = "The Obsidian Tower"
+'i' = "The Crimson Labyrinth"
+'j' = "The Wandering Woodsman"
+'k' = "The Hidden Valley"
+'l' = "Songs of the Forest"
 }
+    }
+    // We then create
 
+
+    void displayGraph
 
 public static void main(final String[] args) {
+
 final Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
-this.graph.put(a, new HashMap<>(Map.of(b, 1, c, 1, d, 1)));
- this.graph.put(b, new HashMap<>(Map.of(e,1 f,1);
-  this.graph.put(c, new HashMap<>(Map.of();
-  this.graph.put(d, new HashMap<>(Map.of();
- this.graph.put(e, new HashMap<>());
-  this.graph.put(f, new HashMap<>(Map.put(h, 1, i, 1));
-  this.graph.put(g, new HashMap<>());
-  this.graph.put(h, new HashMap<>());
- this.graph.put(i, new HashMap<>());
- this.graph.put(j, new HashMap<>(Map.of(k, 1, l, 1));
-this.graph.put(k, new HashMap<>());
- this.graph.put(l, new HashMap<>());
+
+graph.put(a, new HashMap<>(Map.of(b, 1, c, 1, d, 1)));
+graph.put(b, new HashMap<>(Map.of(e,1 f,1);
+graph.put(c, new HashMap<>(Map.of();
+graph.put(d, new HashMap<>(Map.of();
+graph.put(e, new HashMap<>());
+graph.put(f, new HashMap<>(Map.put(h, 1, i, 1));
+graph.put(g, new HashMap<>());
+graph.put(h, new HashMap<>());
+graph.put(i, new HashMap<>());
+graph.put(j, new HashMap<>(Map.of(k, 1, l, 1));
+graph.put(k, new HashMap<>());
+graph.put(l, new HashMap<>());
+
 }
